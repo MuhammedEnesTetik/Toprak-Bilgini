d13cb13bdc70781054be657db57d03ccb5b99c46
MuhammedEnesTetik <106754348+MuhammedEnesTetik@users.noreply.github.com>
2024-07-22 18:41:36 +0300

    Toprak Bilgini
    Azot, pH değeri ve yağış gibi toprak özelliklerine göre en verimli bitki türünü tahmin eden bir makine öğrenmesi modeli...

---
+++ Toprak_Bilgini/flask_api/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pandas as pd
+import joblib
+from flask_cors import CORS  # type: ignore # CORS modülünü ekleyin
+
+app = Flask(__name__)
+CORS(app)  # CORS'u aktifleştirin
+
+# Modeli yükle
+model = joblib.load(r'C:\Users\Vedat\Desktop\Toprak_Bilgini\flask_api\model.pkl')
+label_encoder = joblib.load(r'C:\Users\Vedat\Desktop\Toprak_Bilgini\flask_api\label_encoder.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    df = pd.DataFrame(data, index=[0])
+    
+    for column in df.columns:
+        if df[column].dtype == object:
+            df[column] = label_encoder.transform(df[column])
+    
+    prediction = model.predict(df)
+    prediction_proba = model.predict_proba(df)
+
+    response = {
+        'prediction': label_encoder.inverse_transform(prediction)[0],
+        'probability': round(np.max(prediction_proba) * 100, 2)
+    }
+    
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
